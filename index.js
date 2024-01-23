54662897dc7f606cdb39c479645858d9cf357484
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-01-23 19:24:00 +0530

    add commit

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const yearEl=document.getElementById("year")
 const monthEl=document.getElementById("month")
 const daysEl=document.getElementById("day")
 const hourEl=document.getElementById("hour")
-const mint=document.getElementById("mint")
+const mintEl=document.getElementById("mint")
+const secondEl=document.getElementById("second")
 
 
 
@@ -27,11 +28,32 @@ const toggleDOBSelector=()=>{
     console.log("Toggle", isDOBOpen);
 }
 
+const updateAge=()=>{
+    const currentDate=new Date();
+    const dateDiff=currentDate-dateOfBirth;
+    const year=Math.floor(dateDiff/(1000 * 60 * 60 * 24 * 365));
+    const month=Math.floor(dateDiff/(1000 * 60 * 60 * 24 * 365)) % 12;
+    const day=Math.floor(dateDiff/(1000 * 60 * 60 * 24)) % 30;
+    const hour=Math.floor(dateDiff/(1000 * 60 * 60)) % 60;
+    const mint=Math.floor(dateDiff/ 1000 ) % 60;
+    const second=Math.floor(dateDiff/ 1000 ) % 60;
+    
+    yearEl.innerHTML=year;
+    monthEl.innerHTML=month;
+    daysEl.innerHTML=day;
+    hourEl.innerHTML=hour;
+    mintEl.innerHTML=mint;
+    secondEl.innerHTML=second;
+}
+
 const setDOBHandler=()=>{
-    dateOfBirth = dobInputEl.value;
+    const dateString = dobInputEl.value;
+    dateOfBirth=new Date(dateString)
+
     if(dateOfBirth){
         initialTextEl.classList.add("hide");
         afterDOBBtnTxtEl.classList.remove("hide");
+        setInterval(() => updateAge(),1000);
     }else{
         afterDOBBtnTxtEl.classList.add("hide");
         initialTextEl.classList.remove("hide");
@@ -41,14 +63,7 @@ const setDOBHandler=()=>{
 setDOBHandler();
 
 
-const updateAge=()=>{
-    const currentDate=new Date();
-    console.log(currentDate)
-    const dateDiff=currentDate-dateOfBirth;
-    console.log("date DIFF",dateDiff);
-}
 
-updateAge();
 
 settingContentEl.addEventListener("click",toggleDOBSelector);
 
